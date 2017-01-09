c7e06d29287c251eaf3925446223019f0c436388
Federico Marini <marinif@uni-mainz.de>
2017-01-09 13:46:11 +0100

    volcano plot edited as for plot ma

--- R/plot-volcano.R
+++ R/plot-volcano.R
@@ -1,16 +1,32 @@
 
-plot_volcano <- function(object,
-                         FDR = 0.1,
+plot_volcano <- function(res_obj,
+                         FDR = 0.05,
+                         ylim_up = NULL,
+                         vlines = NULL,
+                         title = NULL,
                          ...) {
 
-  mydf <- as.data.frame(object)
+  mydf <- as.data.frame(res_obj)
   mydf$id <- rownames(mydf)
-  mydf$isDE <- ifelse(is.na(object$padj), FALSE, object$padj < FDR)
+  mydf$isDE <- ifelse(is.na(res_obj$padj), FALSE, res_obj$padj < FDR)
 
-  p <- ggplot(mydf, aes(x = log2FoldChange, y = -log10(pvalue))) + geom_point(aes(color = isDE), alpha = 0.4)
+  mydf <- mydf[mydf$baseMean > 0,]
 
-  p <- p + ylim(0,20)
-  p <- p + theme_bw() + scale_color_manual(values=c("black", "red"))
+  p <- ggplot(mydf, aes_string(x = "log2FoldChange", y = "-log10(pvalue)")) + geom_point(aes_string(color = "isDE"), alpha = 0.4)
+
+  if(!is.null(ylim_up))
+    p <- p + coord_cartesian(ylim = c(0, ylim_up))
+  else
+    p <- p + coord_cartesian(ylim = c(0,20))
+
+  if(!is.null(title))
+    p <- p + ggtitle(title)
+
+  p <- p + theme_bw() +
+    scale_colour_manual(
+      name = paste0("FDR = ",FDR),
+      values = c("black", "red"),
+      labels = c("nonDE","DE"))
 
   p <- p + geom_vline(aes(xintercept = 1), col = "lightblue", alpha = 0.6, size = 1.5) +
     geom_vline(aes(xintercept = -1), col = "lightblue", alpha = 0.6, size = 1.5)
