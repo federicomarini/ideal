2da6ab4e0557cbedf6689779c62b1cd5e32303bb
Federico Marini <marinif@uni-mainz.de>
2017-01-09 13:45:00 +0100

    using base funcs where possible

--- R/res2tbl.R
+++ R/res2tbl.R
@@ -1,33 +1,35 @@
 
-# anno_df$entrez <- mapIds(org.Hs.eg.db,keys = anno_df$gene_id, keytype = "ENSEMBL", column = "ENTREZID")
-# ggss <- rentrez::entrez_summary("gene", anno_df$entrez[1:150])
 
-deseqresult2tbl <-
-  function(deseqresult) {
-    # library("dplyr")
-    if (class(deseqresult) != "DESeqResults") stop("Not a DESeqResults object.")
-    deseqresult <- as.data.frame(deseqresult)
-    deseqresult$id <- rownames(deseqresult)
-    rownames(deseqresult) <- NULL
-    deseqresult <- dplyr::tbl_df(deseqresult)
-    deseqresult <- dplyr::select(deseqresult, id, baseMean, log2FoldChange:padj)
-    dplyr::arrange(deseqresult,padj)
-  }
 
-deseqresult2DEgenes <-
-  function(deseqresult,FDR=0.05) {
-    # library("dplyr")
-    if (class(deseqresult) != "DESeqResults") stop("Not a DESeqResults object.")
-    deseqresult <- as.data.frame(deseqresult)
-    deseqresult$id <- rownames(deseqresult)
-    rownames(deseqresult) <- NULL
-    deseqresult <- dplyr::tbl_df(deseqresult)
-    if("symbol" %in% names(deseqresult))
-      deseqresult <- dplyr::select(deseqresult, id, baseMean, log2FoldChange:symbol)
-    else
-      deseqresult <- dplyr::select(deseqresult, id, baseMean, log2FoldChange:padj)
-    tmp <- dplyr::arrange(deseqresult,padj)
-    res <- tmp[!(is.na(tmp$padj)) & tmp$padj <= FDR,]
-    res
-  }
+deseqresult2tbl <- function(deseqresult) {
+  # library("dplyr")
+  if (class(deseqresult) != "DESeqResults") stop("Not a DESeqResults object.")
+  deseqresult <- as.data.frame(deseqresult)
+
+  deseqresult <- cbind(rownames(deseqresult),deseqresult)
+  names(deseqresult)[1] <- "id"
+
+  dplyr::arrange_(deseqresult,"padj")
+}
+
+deseqresult2DEgenes <- function(deseqresult,
+                                FDR=0.05) {
+  # library("dplyr")
+  if (class(deseqresult) != "DESeqResults") stop("Not a DESeqResults object.")
+  deseqresult <- as.data.frame(deseqresult)
+
+  deseqresult <- cbind(rownames(deseqresult),deseqresult)
+  names(deseqresult)[1] <- "id"
+
+  # deseqresult$id <- rownames(deseqresult)
+  # rownames(deseqresult) <- NULL
+  # deseqresult <- dplyr::tbl_df(deseqresult)
+  # if("symbol" %in% names(deseqresult))
+  #   deseqresult <- dplyr::select(deseqresult, id, baseMean, log2FoldChange:symbol)
+  # else
+  #   deseqresult <- dplyr::select(deseqresult, id, baseMean, log2FoldChange:padj)
+  tmp <- dplyr::arrange_(deseqresult,"padj")
+  res <- tmp[!(is.na(tmp$padj)) & tmp$padj <= FDR,]
+  res
+}
 
