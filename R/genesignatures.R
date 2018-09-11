4c9951f960198a423309eae59f97a3bf1c166681
Federico Marini <marinif@uni-mainz.de>
2018-09-12 00:47:45 +0200

    first working version of signature heatmap is implemented

--- R/genesignatures.R
+++ R/genesignatures.R
@@ -11,6 +11,27 @@ read_gmt <- function(gmtfile){
 }
 
 
-sig_heatmap <- function(vst_data, signature, de_only = TRUE) {
+sig_heatmap <- function(vst_data, my_signature,
+                        de_only = FALSE, annovec, title = "",
+                        cluster_rows = TRUE, cluster_cols = FALSE,
+                        center_mean = TRUE, scale_row = FALSE) {
   
+  mydata <- assay(vst_data)
+  
+  
+  signature_original_ids <- names(annovec)[match(my_signature,annovec)]
+  
+  mydata_sig <- mydata[signature_original_ids,]
+  
+  # to avoid problems later, remove the ones non-expressed and with variance = 0
+  to_remove <- apply(mydata_sig, 1, var) == 0
+  mydata_sig <- mydata_sig[!to_remove,]
+  
+  if(center_mean)
+    mydata_sig <- mydata_sig - rowMeans(mydata_sig)
+  
+  pheatmap(mydata_sig,
+           cluster_rows = cluster_rows, cluster_cols = cluster_cols,
+           scale = ifelse(scale_row,"row","none"),main = title,
+           labels_row = my_signature[!to_remove])
 }
